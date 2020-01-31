dbaaf004892becb8f8800d1fec6e3cb60cf1973c
Jacob Williams <JaWilliamsDev@gmail.com>
2020-01-30 19:26:58 -0500

    Implimented handlesubmit for Login/Register forms

--- src/components/User/GuestRegister.js
+++ src/components/User/GuestRegister.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import axios from "axios";
 import Title from './WelcomeTitle';
 import {FormDiv, Body, Main} from './theme';
 
 
 
 export default function GuestRegister() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('https://workout-journal-backend.herokuapp.com/api/register', values)
+        .then(res => {
+            console.log(res.data)
+            localStorage.setItem('token', res.data.token)
+            props.history.push('/landing');
+        });
+    };
+
     return (
         <Body>
             <Main>
                 <Title/>
                 <FormDiv>
-                    <form>
-                        <input type="text" name="username" placeholder="Enter Username or Email"/>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name="username" placeholder="Enter Username"/>
                         <input type="password" name="password" placeholder="Enter Password"/>
                         <button type="submit">Let's Get Started!</button>
                     </form>
